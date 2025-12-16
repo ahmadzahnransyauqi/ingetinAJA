be2ac87d32066cb098e28362596964c4eca19ea4
rio-johanes <riojohanes08@gmail.com>
2025-12-16 18:26:01 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "https://api-ingetin-aja.vercel.app/api";
+// Hapus pengecekan env untuk sementara agar kita yakin URL-nya benar
+const API_URL = "https://api-ingetin-aja.vercel.app/api"; 
 
 const api = axios.create({
   baseURL: API_URL,
